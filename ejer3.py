7401c03f98933ded2fe2e51a5b9c7eb6f16a527c
1-21-01-22B-13\pc1-21-01-22B-03 <wendy.villegas@epn.edu.ec>
2017-11-30 09:23:50 -0500

    menu

--- ejer3.py
+++ ejer3.py
@@ -3,3 +3,11 @@ print("2. Area del cuadrado")
 print("3. Area del triangulo")
 
 opcion=int(input(print("Ingrese la opcion")))
+
+def circulo(radio):
+    area=(radio*radio)*3.1415
+    return area
+
+if opcion==1:
+    radio=int(input(print("Ingrese el radio")))
+    total= circulo(radio)
